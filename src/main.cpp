3305e5b81edb1532c4417947ace4213bde7496e8
stekap000 <stekap000@gmail.com>
2024-05-09 03:28:08 +0200

    direct lighting testing

--- src/main.cpp
+++ src/main.cpp
@@ -29,7 +29,7 @@ kajiya::Hittable *trace_ray(kajiya::Ray &ray,
 float pi		   = 3.1415926535897932;
 float bias		   = 0.0001;
 int max_depth	   = 3;
-int rays_per_pixel = 50;
+int rays_per_pixel = 10;
 
 kajiya::Spectrum Lr(kajiya::Hittable *object, kajiya::Ray &r,
 					std::vector<kajiya::Hittable *> objects, int depth);
@@ -50,7 +50,7 @@ kajiya::Spectrum Li(kajiya::Ray &ray, std::vector<kajiya::Hittable *> objects,
 				 ? intersection_point.value() - surface_normal * bias
 				 : intersection_point.value() + surface_normal * bias);
 
-		kajiya::Ray new_ray = kajiya::Ray(new_origin, -ray.direction);
+		kajiya::Ray new_ray		 = kajiya::Ray(new_origin, -ray.direction);
 		new_ray.refractive_index = ray.refractive_index;
 
 		kajiya::Spectrum spectrum = closest->material().emittance;
@@ -85,34 +85,102 @@ kajiya::Vec3 visible_light_corner(kajiya::Vec3 point,
 	return p1;
 }
 
+// Hardcoded for testing.
+kajiya::Material LIGHT; 
+float LIGHT_AREA = 13184;
 kajiya::Spectrum Lr(kajiya::Hittable *object, kajiya::Ray &r,
 					std::vector<kajiya::Hittable *> objects, int depth) {
 
 	kajiya::Vec3 new_direction;
-	float inverse_prob = 1;
 	kajiya::Spectrum relevant_object_spectrum;
-	
+	kajiya::Spectrum direct_light_contribution;
+	float brdf_over_probability		   = 1;
+	float brdf = 1; // Move this to material.
+	float probability = 1;
+	float preservation		   = 0.8f;
+	if (depth == max_depth - 1)
+		preservation = 1;
 
-	if(object->material().type == kajiya::Material::metal) {
-		inverse_prob = 1;
+	// Direct light sampling (just one light for now).
+	if (object->material().type != kajiya::Material::light) {
+#if 1 // Probabilistic light sampling
+		float rand_light_z = 228 + (331 - 228) * rand_float();
+		float rand_light_x = 214 + (342 - 214) * rand_float();
+		kajiya::Vec3 light_dist_vec = kajiya::Vec3(rand_light_x, 548.8, rand_light_z) - r.origin;
+		kajiya::Ray light_ray(r.origin, light_dist_vec.unit());
+		
+		auto closest_temp = trace_ray(light_ray, objects);
+		auto intersection_point_temp = closest_temp->intersect(light_ray);
+		
+		if(intersection_point_temp.has_value()) {
+			if(closest_temp->material().type == kajiya::Material::light) {
+				float light_dist_squared = light_dist_vec.norm_squared();
+				float light_cos = kajiya::Vec3::dot(-light_ray.direction, kajiya::Vec3(0, -1, 0));
+				//float light_direction_probability = LIGHT_AREA * light_cos / (2 * pi);
+				//float light_direction_probability = 1.f / (LIGHT_AREA * light_cos);
+				
+				// Looks better with this probability that with other ones (why???).
+				float light_direction_probability = 1;
+				
+				direct_light_contribution = LIGHT.emittance * LIGHT_AREA * light_cos / light_dist_squared / light_direction_probability;
+			}
+		}
+#else // Deterministic light sampling (slower)
+		int hits		   = 0;
+		float light_dist_squared = 0;
+		float light_cos	   = 0;
+
+		for (int i = 0; i < light_mesh.size(); ++i) {
+			for (int j = 0; j < light_mesh[0].size(); ++j) {
+				kajiya::Vec3 light_dist_vec = light_mesh[i][j] - r.origin;
+				kajiya::Ray light_ray(r.origin, light_dist_vec.unit());
+
+				light_dist_squared += light_dist_vec.norm_squared();
+				light_cos +=
+					kajiya::Vec3::dot(-light_ray.direction, kajiya::Vec3(0, -1, 0));
+
+				auto closest = trace_ray(light_ray, objects);
+
+				if (closest->material().type == kajiya::Material::light) {
+					hits++;
+					new_direction = light_mesh[i][j] - r.origin;
+				}
+			}
+		}
+
+		light_dist_squared /= ((light_mesh.size() + 1) * (light_mesh[0].size() + 1));
+		light_cos /= ((light_mesh.size() + 1) * (light_mesh[0].size() + 1));
+
+		float direct_light_scaling =
+			static_cast<float>(hits) /
+			((light_mesh.size() + 1) * (light_mesh[0].size() + 1));
+
+		direct_light_contribution = LIGHT.emittance * LIGHT_AREA * light_cos / light_dist_squared * direct_light_scaling;
+#endif
+	}
+
+	if (object->material().type == kajiya::Material::metal) {
+		probability = 1;
+		brdf = 1;
 		kajiya::Vec3 surface_normal = object->normal(r.origin);
-		new_direction = (r.direction).reflect_around(surface_normal);
+		new_direction			 = (r.direction).reflect_around(surface_normal);
 		relevant_object_spectrum = object->material().reflectance;
-    }
-	else if(object->material().type == kajiya::Material::glass) {
+	} else if (object->material().type == kajiya::Material::glass) {
 		kajiya::Vec3 surface_normal = object->normal(r.origin);
-		float refractive_index_1 = r.refractive_index;
-		float refractive_index_2 = object->material().refractive_index;
+		float refractive_index_1	= r.refractive_index;
+		float refractive_index_2	= object->material().refractive_index;
 
-		float fresnel_reflection_coef = fresnel(-r.direction, surface_normal, refractive_index_1, refractive_index_2);
-		if(rand_float() < fresnel_reflection_coef) {
-			inverse_prob = 1.f / fresnel_reflection_coef;
+		float fresnel_reflection_coef =
+			fresnel(-r.direction, surface_normal, refractive_index_1,
+					refractive_index_2);
+		if (rand_float() < fresnel_reflection_coef) {
+			brdf_over_probability  = 1.f / fresnel_reflection_coef;
 			new_direction = (r.direction).reflect_around(surface_normal);
 			relevant_object_spectrum = object->material().reflectance;
-		}
-		else {
-			inverse_prob = 1.f / (1 - fresnel_reflection_coef);
-			inverse_prob *= 0.006; // Experimentation.
+		} else {
+			probability = 1 - fresnel_reflection_coef;
+			brdf = 1.f;
+			brdf *= 0.007; // Experimentation.
 
 			// Handle case when ray travels within object.
 			bool ray_is_internal_to_object =
@@ -123,48 +191,50 @@ kajiya::Spectrum Lr(kajiya::Hittable *object, kajiya::Ray &r,
 
 			float refractive_index = refractive_index_1 / refractive_index_2;
 			float C1 = kajiya::Vec3::dot(r.direction, surface_normal) /
-				(r.direction.norm());
+					   (r.direction.norm());
 			float total_internal_reflection_indicator =
 				1 - refractive_index * refractive_index * (1 - C1 * C1);
 			float C2 = std::sqrt(total_internal_reflection_indicator);
 
 			// Form refraction ray.
-			new_direction =
-				(-r.direction * refractive_index +
-				 surface_normal * (refractive_index * C1 - C2))
-				.unit();
+			new_direction = (-r.direction * refractive_index +
+							 surface_normal * (refractive_index * C1 - C2))
+								.unit();
 
 			r.direction = -r.direction;
-			std::optional<kajiya::Vec3> intersection_point_opt = object->intersect(r);
+			std::optional<kajiya::Vec3> intersection_point_opt =
+				object->intersect(r);
 			kajiya::Vec3 intersection_point;
-			if(intersection_point_opt.has_value()) {
+			if (intersection_point_opt.has_value()) {
 				intersection_point = intersection_point_opt.value();
 			}
 
-			r.origin =
-				(kajiya::Vec3::dot(new_direction, surface_normal) < 0
-				 ? intersection_point - surface_normal * bias
-				 : intersection_point + surface_normal * bias);
-			
+			r.origin = (kajiya::Vec3::dot(new_direction, surface_normal) < 0
+							? intersection_point - surface_normal * bias
+							: intersection_point + surface_normal * bias);
+
 			// If there is no total internal reflection.
 			r.refractive_index = refractive_index_2;
 
 			relevant_object_spectrum = object->material().transmittance;
 		}
-	}
-	else {
+	} else {
 		new_direction =
 			rand_unit_vector_on_hemisphere(object->normal(r.origin));
-		inverse_prob = 2;
+		probability = 1.f / (2 * pi);
+		brdf = 1.f / pi;
 		relevant_object_spectrum = object->material().reflectance;
 	}
 
 	kajiya::Ray new_ray(r.origin, new_direction);
 	new_ray.refractive_index = r.refractive_index;
 
-	return relevant_object_spectrum * Li(new_ray, objects, depth) * inverse_prob *
-		   kajiya::Vec3::dot(object->normal(new_ray.origin).unit(),
-							 new_ray.direction.unit());
+	float Li_dot = kajiya::Vec3::dot(object->normal(new_ray.origin).unit(),
+					  new_ray.direction.unit());
+
+	// Weird dots come from Li (can be checked by multiplying by probability instead of dividing). We can this to also see what image looks like without Li, but with direct lighting.
+	return relevant_object_spectrum * brdf * preservation *
+		(direct_light_contribution + Li(new_ray, objects, depth) * Li_dot / probability);
 }
 
 void generate_light_mesh(std::vector<std::vector<kajiya::Vec3>> &points, int n1,
@@ -173,6 +243,7 @@ void generate_light_mesh(std::vector<std::vector<kajiya::Vec3>> &points, int n1,
 	kajiya::Vec3 p2(342.0, 548.8, 331.0);
 	kajiya::Vec3 p3(214.0, 548.8, 331.0);
 	kajiya::Vec3 p4(214.0, 548.8, 228.0);
+	
 
 	kajiya::Vec3 d1 = (p2 - p1);
 	kajiya::Vec3 d2 = (p4 - p1);
@@ -196,7 +267,8 @@ int main() {
 
 	std::vector<kajiya::Hittable *> objects;
 
-	generate_light_mesh(light_mesh, 5, 5);
+	generate_light_mesh(light_mesh, 2, 2);
+	LIGHT = kajiya::Material::get_light();
 
 	// floor
 	kajiya::Rectangle floor(
@@ -291,8 +363,8 @@ int main() {
 		kajiya::Material::get_white());
 	objects.push_back(&tall_block_front);
 
-	const unsigned width  = 400;
-	const unsigned height = 400;
+	const unsigned width  = 300;
+	const unsigned height = 300;
 	float half_width	  = width / 2.f;
 	float half_height	  = height / 2.f;
 
