d6966a5fee0d9017380796f44f5c25babe4886e5
stekap000 <stekap000@gmail.com>
2024-05-18 20:07:09 +0200

    loading with polygon triangulation

--- src/loader.h
+++ src/loader.h
@@ -5,6 +5,7 @@
 #include "hittable.h"
 #include "material.h"
 #include "rectangle.h"
+#include "mesh.h"
 #include "vec3.h"
 #include <vector>
 #include <string>
@@ -18,7 +19,7 @@ class Loader {
 public:
 	static std::vector<Triangle*> load_obj(std::string inputfile) {
 		tinyobj::ObjReaderConfig reader_config;
-		reader_config.mtl_search_path = "./";
+		reader_config.mtl_search_path = "./assets";
 
 		tinyobj::ObjReader reader;
 
@@ -38,40 +39,37 @@ public:
 
 		std::vector<Triangle*> triangles;
 		Triangle temp;
+		temp.m = Material::get_white();
 
 		// Loop over shapes
-		for(size_t s = 0; s < shapes.size(); s++) {
+		for(size_t shape_index = 0; shape_index < shapes.size(); shape_index++) {
 			size_t index_offset = 0;
 
 			// Loop over faces
-			for(size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
-				size_t fv = size_t(shapes[s].mesh.num_face_vertices[f]);
+			for(size_t face_index = 0; face_index < shapes[shape_index].mesh.num_face_vertices.size(); face_index++) {
+				size_t current_face_vertices_num = size_t(shapes[shape_index].mesh.num_face_vertices[face_index]);
 
-				// Loop over face vertices
-				for(size_t v = 0; v < fv; v++) {
-					tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
-					tinyobj::real_t vx = attrib.vertices[3*size_t(idx.vertex_index)+0];
-					tinyobj::real_t vy = attrib.vertices[3*size_t(idx.vertex_index)+1];
-					tinyobj::real_t vz = attrib.vertices[3*size_t(idx.vertex_index)+2];
+				// NOTE(stekap): Not storage efficient.
+				for(int i = 1; i < current_face_vertices_num - 1; ++i) {
+					tinyobj::index_t idx_1 = shapes[shape_index].mesh.indices[index_offset + 0];
+					tinyobj::index_t idx_2 = shapes[shape_index].mesh.indices[index_offset + i];
+					tinyobj::index_t idx_3 = shapes[shape_index].mesh.indices[index_offset + i + 1];
+					
+					temp.p1 = Vec3(attrib.vertices[3*size_t(idx_1.vertex_index) + 0],
+								   attrib.vertices[3*size_t(idx_1.vertex_index) + 1],
+								   attrib.vertices[3*size_t(idx_1.vertex_index) + 2]);
+					temp.p2 = Vec3(attrib.vertices[3*size_t(idx_2.vertex_index) + 0],
+								   attrib.vertices[3*size_t(idx_2.vertex_index) + 1],
+								   attrib.vertices[3*size_t(idx_2.vertex_index) + 2]);
+					temp.p3 = Vec3(attrib.vertices[3*size_t(idx_3.vertex_index) + 0],
+								   attrib.vertices[3*size_t(idx_3.vertex_index) + 1],
+								   attrib.vertices[3*size_t(idx_3.vertex_index) + 2]);
 
-					tinyobj::real_t nx = 0, ny = 0, nz = 0;
-					// Check if `normal_index` is zero or positive. negative = no normal data
-					if (idx.normal_index >= 0) {
-						nx = attrib.normals[3*size_t(idx.normal_index)+0];
-						ny = attrib.normals[3*size_t(idx.normal_index)+1];
-						nz = attrib.normals[3*size_t(idx.normal_index)+2];
-					}
-
-					if(v % 3 == 0) {
-						temp.p1 = Vec3(vx, vy, vz);
-					}
-					else if(v % 3 == 1) {
-						temp.p2 = Vec3(vx, vy, vz);
-					}
-					else {
-						temp.p3 = Vec3(vx, vy, vz);
-						temp.n = Vec3(nx, ny, nz);
-						triangles.push_back(new Triangle(temp.p1, temp.p2, temp.p3, temp.n, Material::get_white()));
+					// NOTE(stekap): For now, we take normal at one point and set it as triangle normal.
+					if(idx_1.normal_index >= 0) {
+						temp.n = Vec3(attrib.normals[3*size_t(idx_1.normal_index) + 0],
+									  attrib.normals[3*size_t(idx_1.normal_index) + 1],
+									  attrib.normals[3*size_t(idx_1.normal_index) + 2]);
 					}
 
 					// Check if `texcoord_index` is zero or positive. negative = no texcoord data
@@ -84,8 +82,15 @@ public:
 					// tinyobj::real_t red   = attrib.colors[3*size_t(idx.vertex_index)+0];
 					// tinyobj::real_t green = attrib.colors[3*size_t(idx.vertex_index)+1];
 					// tinyobj::real_t blue  = attrib.colors[3*size_t(idx.vertex_index)+2];
+
+					triangles.push_back(new Triangle(temp.p1, temp.p2, temp.p3, temp.n, temp.m));
 				}
-				index_offset += fv;
+
+				// TODO(stekap): More efficient way to represent meshes.
+				// TODO(stekap): Incorporate per vertex normals.
+				// TODO(stekap): Handle material and color.
+
+				index_offset += current_face_vertices_num;
 			}
 		}
 
